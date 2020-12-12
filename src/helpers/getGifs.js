7207124004696adabcade70e2e1c008205a34bcd
Pedro Flores Cortes <pedro.flores@jarbrect.com.mx>
2020-12-12 00:27:21 -0600

    arreglando el fetch

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 
 export const getGifs = async ( category ) => {
-    const url = `https:api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=AzQK4mitwh1RHR31Hm2cKTTWKjDJj5Nk`
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=10&api_key=AzQK4mitwh1RHR31Hm2cKTTWKjDJj5Nk`
     const respuesta = await fetch( url );
     const {data} = await respuesta.json();
     const gifs = data.map( img => {
