d27feceefa83d78c9c636a2f85d23ad9a38ca2e6
Pedro Flores Cortes <pedro.flores@jarbrect.com.mx>
2020-12-11 23:46:53 -0600

    Se termino

---
+++ src/GitExpertApp.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import AddCategory from './components/AddCategory'
+import { GifGrid } from './components/GifGrid';
+
+
+const GitExpertApp = () => {
+
+    //const categories = ['One Punch','Samurai X','Dragon Ball'];
+    const [categories, setCategories] = useState(['One Punch']);
+
+    /* const handleAdd = () => {
+        setCategories( [...categories,'Don gato'] )
+    } */
+
+    return (
+        <>
+            <h2>GitExpertApp</h2>
+            <AddCategory setCategories={ setCategories }/>
+            <hr />
+            <ol>
+                {
+                    categories.map(category => 
+                        <GifGrid 
+                            key={category}
+                            category={category}     
+                        />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GitExpertApp;
